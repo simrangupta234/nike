e69fbff21b8d067d9de7d6b16f8fa19ac01b18bf
unknown <simrangupta172002@gmail.com>
2024-08-06 10:09:30 +0530

    project made

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Nav from "../components/Nav";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+  CustomerReviews,
+  Footer,
+  Subscribe,
+};
